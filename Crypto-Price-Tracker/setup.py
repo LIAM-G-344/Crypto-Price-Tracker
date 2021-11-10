04a3b988cc24a2414ccdee0076083af2d31ef40b
LIAM-G-344 <93160178+LIAM-G-344@users.noreply.github.com>
2021-11-10 10:39:52 -0500

    Update setup.py

--- Crypto-Price-Tracker/setup.py
+++ Crypto-Price-Tracker/setup.py
@@ -1,5 +1,21 @@
 import sys
 import subprocess
+
+#Copyright (C) 2021  LIAM-G-344
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see https://github.com/LIAM-G-344/Crypto-Price-Tracker/blob/main/LICENSE.
+
 print("Setup may install as many as 8 packages. If setup.py was not ran with administartor privlages the instalation will fail.")
 inp_1 = input("To contine press y/n: ")
 
